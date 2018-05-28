412e195f1134328b81e98b1bd5516bb0a130982b
SEO Themes <seothemeswp@gmail.com>
2018-05-28 17:30:51 +0800

    Update split.sh

--- split.sh
+++ split.sh
@@ -6,19 +6,12 @@ touch ./scss/index.scss
 input=$1
 filename='partial'
 
-# Splits file at '/* #' string into partials.
+# Splits file at regex string.
 split -a 1 -p '/\*\ #\ ' $input $filename
 
-# Counter.
-n=1
-
-# Add file suffix.
-# for file in partial*; do
-#    mv "$file" "${file:0:7}$((n++)).scss";
-# done
-
+# Rename and move partials.
 for file in partial*; do
     read -r firstline<$file;
     sudo mv "$file" `echo ./scss/${firstline//[^[:alnum:]]/}.scss | tr '[A-Z]' '[a-z]'`;
-    echo @import \"${firstline//[^[:alnum:]]/}\" | tr '[A-Z]' '[a-z]' >> ./scss/index.scss
+    echo @import \"${firstline//[^[:alnum:]]/}\"; | tr '[A-Z]' '[a-z]' >> ./scss/index.scss
 done
